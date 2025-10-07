2df9514ed6aa6b7572e815e74e212f244f6bcbb9
Maksim Vish <maksvish135@yandex.ru>
2025-10-07 13:36:16 +0300

    Reset project to empty for prac4

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:project/calculator_screen.dart';
-import 'package:project/crocodile_screen.dart';
-import 'package:project/gif_screen.dart';
-import 'package:project/home_screen.dart';
-import 'package:project/list_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
       ),
-      home: const MyHomePage(title: 'Flutter Home Page'),
+      home: const MyHomePage(title: 'Flutter Prac 4'),
     );
   }
 }
@@ -36,42 +31,13 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _selectedIndex = 0;
-
-  final List<Widget> screens = [
-    const HomeScreen(),
-    CrocodileScreen(),
-    CalculatorScreen(),
-    const GifScreen(),
-    const ListScreen(),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(backgroundColor: Colors.amber, title: Text(widget.title)),
-      body: screens[_selectedIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Начало"),
-          BottomNavigationBarItem(icon: Icon(Icons.pets), label: "Крокодилы"),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.calculate),
-            label: "Калькулятор",
-          ),
-          BottomNavigationBarItem(icon: Icon(Icons.gif), label: "Gif"),
-          BottomNavigationBarItem(icon: Icon(Icons.list), label: "Список"),
-        ],
-        type: BottomNavigationBarType.fixed,
-        onTap: _onItemTapped,
-        currentIndex: _selectedIndex,
-      ),
+      body: Text(widget.title),
     );
   }
+
+
 }
