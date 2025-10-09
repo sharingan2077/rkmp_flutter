b6ee2abc5fe5880593d69d55051448c92e1228cb
Maksim Vish <maksvish135@yandex.ru>
2025-10-09 12:24:07 +0300

    [prac5] Add empty stats_card and status_chip

---
+++ lib/features/crocodiles/widgets/stats_card.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class StatsCard extends StatelessWidget {
+  const StatsCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/features/crocodiles/widgets/status_chip.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class StatusChip extends StatelessWidget {
+  const StatusChip({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
