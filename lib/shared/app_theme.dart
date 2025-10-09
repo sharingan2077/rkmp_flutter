0504e988bcc7937c4f20d98869ec47942e6a7af5
Maksim Vish <maksvish135@yandex.ru>
2025-10-09 11:52:21 +0300

    [prac5] Add shared/app_theme

---
+++ lib/shared/app_theme.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static final lightTheme = ThemeData(
+    primarySwatch: Colors.green,
+    useMaterial3: true,
+  );
+
+  static final darkTheme = ThemeData(
+    primarySwatch: Colors.green,
+    brightness: Brightness.dark,
+    useMaterial3: true,
+  );
+}
