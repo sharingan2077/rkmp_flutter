20809102c8542b9b606b080113ee8442bda4981c
Maksim Vish <maksvish135@yandex.ru>
2025-12-13 08:16:32 +0300

    [prac10] реализация бизнес экрана учета питания

--- lib/features/food/screens/crocodile_food_screen.dart
+++ lib/features/food/screens/crocodile_food_screen.dart
@@ -1,4 +1,3 @@
-// features/food/screens/crocodile_food_screen.dart
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
